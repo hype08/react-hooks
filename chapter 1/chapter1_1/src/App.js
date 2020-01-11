dfb48cbda65b4e15f11f5eb5005e7fb0418d640f
henry <tarou.88@gmail.com>
2020-01-11 13:42:05 +0900

    add input and handleChange

--- chapter 1/chapter1_1/src/App.js
+++ chapter 1/chapter1_1/src/App.js
@@ -3,11 +3,21 @@ import React, { Component } from "react";
 class MyName extends Component {
   constructor(props) {
     super(props);
-    this.state = (name: "");
+    this.state = { name: "" };
   }
 
   handleChange(e) {
     this.setState({ name: e.target.value });
+  }
+
+  render() {
+    const { name } = this.state;
+    return (
+      <div>
+        <h1>My name is: {name}</h1>
+        <input type="text" value={name} onChange={this.handleChange} />
+      </div>
+    );
   }
 }
 
