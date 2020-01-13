1b48d92db66c06bcd7a675916e3fa5c3941b6193
henry <tarou.88@gmail.com>
2020-01-13 10:43:08 +0900

    3_1 Register

--- 3_blog-app/src/App.js
+++ 3_blog-app/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
 // import Login from './user/Login';
-import Logout from './user/Logout';
+// import Logout from './user/Logout';
+import Register from './user/Register';
 
 function App() {
-  return <Logout user="Henry"/>
+  return <Register />
 }
 
 export default App;
