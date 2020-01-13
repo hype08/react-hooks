eece4be7db212910b30b43b3b0f1c5b58b043c1d
henry <tarou.88@gmail.com>
2020-01-13 22:52:54 +0900

    3_3 register component username

--- 3_blog-app/3_3/src/user/Register.js
+++ 3_blog-app/3_3/src/user/Register.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from "react";
 
-export default function Register() {
+export default function Register({ setUser }) {
+  const [username, setUsername] = useState("");
+
+  function handleUsername(e) {
+    setUsername(e.target.value);
+  }
   return (
-    <form onSubmit={e => e.preventDefault()}>
+    <form
+      onSubmit={e => {
+        e.preventDefault();
+        setUser(username);
+      }}
+    >
       <label htmlFor="register-username">Username: </label>
-      <input type="text" name="register-username" id="register-username"/>
+      <input
+        type="text"
+        onChange={handleUsername}
+        name="register-username"
+        id="register-username"
+      />
       <label htmlFor="register-password">Password: </label>
-      <input type="text" name="register-password" id="register-password"/>
+      <input type="text" name="register-password" id="register-password" />
       <label htmlFor="register-password-repeat">Repeat password: </label>
-      <input type="text" name="register-password-repeat" id="register-password-repeat"/>
-      <input type="submit" value="Register"/>
+      <input
+        type="text"
+        name="register-password-repeat"
+        id="register-password-repeat"
+      />
+      <input type="submit" value="Register" disabled={username.length === 0} />
     </form>
-  )
-};
+  );
+}
