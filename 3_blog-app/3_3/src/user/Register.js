c8d38416ab531935a3b6c49795b3fc415fff4d3f
henry <tarou.88@gmail.com>
2020-01-13 23:27:56 +0900

    3_3 register component match passwords

--- 3_blog-app/3_3/src/user/Register.js
+++ 3_blog-app/3_3/src/user/Register.js
@@ -2,10 +2,21 @@ import React, { useState } from "react";
 
 export default function Register({ setUser }) {
   const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordRepeat, setPasswordRepeat] = useState("");
 
   function handleUsername(e) {
     setUsername(e.target.value);
   }
+
+  function handlePassword(e) {
+    setPassword(e.target.value);
+  }
+
+  function handlePasswordRepeat(e) {
+    setPasswordRepeat(e.target.value);
+  }
+
   return (
     <form
       onSubmit={e => {
@@ -21,14 +32,26 @@ export default function Register({ setUser }) {
         id="register-username"
       />
       <label htmlFor="register-password">Password: </label>
-      <input type="text" name="register-password" id="register-password" />
+      <input
+        type="text"
+        value={password}
+        onChange={handlePassword}
+        name="register-password"
+        id="register-password"
+      />
       <label htmlFor="register-password-repeat">Repeat password: </label>
       <input
         type="text"
+        value={passwordRepeat}
+        onChange={handlePasswordRepeat}
         name="register-password-repeat"
         id="register-password-repeat"
       />
-      <input type="submit" value="Register" disabled={username.length === 0} />
+      <input
+        type="submit"
+        value="Register"
+        disabled={username.length === 0 || passwordRepeat !== password}
+      />
     </form>
   );
 }
