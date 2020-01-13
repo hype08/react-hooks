0225ed989646ac76322f6ddc635f730c7ae799f2
henry <tarou.88@gmail.com>
2020-01-13 11:18:15 +0900

    3_2 Post component

--- 3_blog-app/src/App.js
+++ 3_blog-app/src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
+
 import "./App.css";
 import UserBar from "./user/UserBar";
+import Post from "./post/Post";
 
 function App() {
-  return <UserBar />;
+  return (
+    <>
+      <UserBar />
+      <Post
+        title="React Hooks"
+        content="The greatest thing since sliced bread"
+        author="Henry"
+      />
+    </>
+  );
 }
 
 export default App;
