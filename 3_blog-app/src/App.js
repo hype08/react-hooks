4bc19688b74acca67a08a9d593eaf2096b24d2ea
henry <tarou.88@gmail.com>
2020-01-13 21:27:54 +0900

    3_2 static app

--- 3_blog-app/src/App.js
+++ 3_blog-app/src/App.js
@@ -5,6 +5,7 @@ import UserBar from "./user/UserBar";
 import CreatePost from "./post/CreatePost";
 import PostList from "./post/PostList";
 
+const user = "Henry";
 const posts = [
   {
     title: "React Hooks",
@@ -20,10 +21,13 @@ const posts = [
 
 export default function App() {
   return (
-    <>
-      <UserBar />
-      <CreatePost />
+    <div style={{ padding: 8 }}>
+      <UserBar user={user} />
+      <br />
+      <CreatePost user={user} />
+      <br />
+      <hr />
       <PostList posts={posts} />
-    </>
+    </div>
   );
 }
--- 3_blog-app/src/user/UserBar.js
+++ 3_blog-app/src/user/UserBar.js
@@ -4,9 +4,7 @@ import Login from "./Login";
 import Register from "./Register";
 import Logout from "./Logout";
 
-export default function UserBar() {
-  const user = "";
-
+export default function UserBar({ user }) {
   if (user) {
     return <Logout user={user} />;
   } else {
