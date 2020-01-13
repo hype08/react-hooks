bf207ccf2c02adaece4f8a12c2121103b4d805ed
henry <tarou.88@gmail.com>
2020-01-13 10:22:07 +0900

    3_1 Logout component

--- 3_blog-app/src/App.js
+++ 3_blog-app/src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import './App.css';
-import Login from './user/Login';
+// import Login from './user/Login';
+import Logout from './user/Logout';
 
 function App() {
-  return (
-    <div className="App">
-      <Login /> 
-    </div>
-  );
+  return <Logout user="Henry"/>
 }
 
 export default App;
