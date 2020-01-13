090f69d32dbe9d75cc2ebfafc80fd8b8ecba0c6c
henry <tarou.88@gmail.com>
2020-01-13 22:09:16 +0900

    ???

--- 3_blog-app/3_3/src/user/Login.js
+++ 3_blog-app/3_3/src/user/Login.js
@@ -1,19 +1,30 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
-export default function Login ({setUser}) {
-  const [username, setUsername] = useState("")
+export default function Login({ setUser }) {
+  const [username, setUsername] = useState("");
 
   function handleUsername(e) {
-    setUsername(e.target.value)
+    setUsername(e.target.value);
   }
 
   return (
-    <form onSubmit={e => {e.preventDefault(); setUser(username)}}>
+    <form
+      onSubmit={e => {
+        e.preventDefault();
+        setUser(username);
+      }}
+    >
       <label htmlFor="login-username">Username: </label>
-      <input type="text" value={username} name="login-username" id="login-username" onChange={handleUsername}/>
+      <input
+        type="text"
+        value={username}
+        name="login-username"
+        id="login-username"
+        onChange={handleUsername}
+      />
       <label htmlFor="login-password">Password: </label>
       <input type="text" name="login-password" id="login-password" />
-      <input type="submit" value="Login" disabled={username.length === 0}/>
+      <input type="submit" value="Login" disabled={username.length === 0} />
     </form>
-  )
+  );
 }
