24bb5b65e9d4101d8cc2f32b66c3d2e33c2f00fe
henry <tarou.88@gmail.com>
2020-01-21 08:29:08 +0900

    use Context for User-related components

--- 5_react_context/5_3/src/user/UserBar.js
+++ 5_react_context/5_3/src/user/UserBar.js
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
+import { StateContext } from "../contexts";
 
 import Login from "./Login";
 import Register from "./Register";
 import Logout from "./Logout";
 
-export default function UserBar({ user, dispatch }) {
+export default function UserBar() {
+  // grab state from state object in contexts
+  const { state } = useContext(StateContext);
+  // de-structure state to get user
+  const { user } = state;
+
   if (user) {
-    return <Logout user={user} dispatch={dispatch} />;
+    return <Logout />;
   } else {
     return (
       <>
-        <Login dispatch={dispatch} />
-        <Register dispatch={dispatch} />
+        <Login />
+        <Register />
       </>
     );
   }
