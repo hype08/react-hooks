5b6ef75c1565e5c30bec58d19146a7877505b22a
henry <tarou.88@gmail.com>
2020-01-21 12:49:38 +0900

    init 6_1

---
+++ 6_requests-suspense/6_1/src/reducers.js
@@ -0,0 +1,32 @@
+export default function appReducer(state, action) {
+  return {
+    user: userReducer(state.user, action),
+    posts: postsReducer(state.posts, action)
+  };
+}
+
+function postsReducer(state, action) {
+  switch (action.type) {
+    case "CREATE_POST":
+      const newPost = {
+        title: action.title,
+        content: action.content,
+        author: action.author
+      };
+      return [newPost, ...state];
+    default:
+      return state;
+  }
+}
+
+function userReducer(state, action) {
+  switch (action.type) {
+    case "LOGIN":
+    case "REGISTER":
+      return action.username;
+    case "LOGOUT":
+      return "";
+    default:
+      return state;
+  }
+}
---
+++ 6_requests-suspense/README.md
@@ -0,0 +1,14 @@
+# Chapter 6 - Implementing Requests and React Suspense
+
+## Getting Started & Install
+
+```
+cd 6_1 && npm install
+```
+
+## Run
+
+```
+npm run start
+```
+
