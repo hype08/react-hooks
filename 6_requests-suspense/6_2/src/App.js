e96261d167f8d4d93e3b06bc2bb468e8d5fe195d
henry <tarou.88@gmail.com>
2020-01-23 13:06:59 +0900

    init 6_2

---
+++ 6_requests-suspense/6_2/src/App.js
@@ -0,0 +1,63 @@
+import React, { useReducer, useEffect, useState } from "react";
+
+import "./App.css";
+import UserBar from "./user/UserBar";
+import CreatePost from "./post/CreatePost";
+import PostList from "./post/PostList";
+import Header from "./Header";
+import ChangeTheme from "./ChangeTheme";
+import { ThemeContext, StateContext } from "./contexts";
+import appReducer from "./reducers";
+
+const defaultPosts = [
+  {
+    title: "React Hooks",
+    content: "The greatest thing since sliced bread",
+    author: "Henry"
+  },
+  {
+    title: "React Fragments",
+    content: "The greatest thing since sliced bread",
+    author: "Henry"
+  }
+];
+
+export default function App() {
+  const [theme, setTheme] = useState({
+    primaryColor: "deepskyblue",
+    secondaryColor: "coral"
+  });
+
+  const [state, dispatch] = useReducer(appReducer, {
+    user: "",
+    posts: defaultPosts
+  });
+
+  const { user } = state;
+
+  useEffect(() => {
+    if (user) {
+      document.title = `${user} - React Hooks Blog`;
+    } else {
+      document.title = `React Hooks Blog`;
+    }
+  }, [user]);
+
+  return (
+    <StateContext.Provider value={{ state, dispatch }}>
+      <ThemeContext.Provider value={theme}>
+        <div style={{ padding: 8 }}>
+          <Header text="React Hooks Blog" />
+          <ChangeTheme theme={theme} setTheme={setTheme} />
+          <br />
+          <UserBar />
+          <br />
+          {user && <CreatePost />}
+          <br />
+          <hr />
+          <PostList />
+        </div>
+      </ThemeContext.Provider>
+    </StateContext.Provider>
+  );
+}
