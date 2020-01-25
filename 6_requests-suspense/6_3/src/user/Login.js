ec77fc7800e73da386ad57815badf362f61be788
henry <tarou.88@gmail.com>
2020-01-25 10:52:14 +0900

    6_3 password state and failed login state

--- 6_requests-suspense/6_3/src/user/Login.js
+++ 6_requests-suspense/6_3/src/user/Login.js
@@ -1,12 +1,19 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
+import { useResource } from "react-request-hook";
 import { StateContext } from "../contexts";
 
 export default function Login() {
   const [username, setUsername] = useState("");
+  const [loginFailed, setLoginFailed] = useState(false);
+  const [password, setPassword] = useState("");
   const { dispatch } = useContext(StateContext);
 
   function handleUsername(e) {
     setUsername(e.target.value);
+  }
+
+  function handlePassword(e) {
+    setPassword(e.target.value);
   }
 
   return (
@@ -25,7 +32,13 @@ export default function Login() {
         onChange={handleUsername}
       />
       <label htmlFor="login-password">Password: </label>
-      <input type="text" name="login-password" id="login-password" />
+      <input
+        type="password"
+        value={password}
+        onChange={handlePassword}
+        name="login-password"
+        id="login-password"
+      />
       <input type="submit" value="Login" disabled={username.length === 0} />
     </form>
   );
