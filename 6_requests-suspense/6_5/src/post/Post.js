b3440f1edc1d931b7f3cf6e3860554781974a964
henry <tarou.88@gmail.com>
2020-01-26 22:02:07 +0900

    6_5 React.memo for Post component so logging in does not cause the Post to re-render

--- 6_requests-suspense/6_5/src/post/Post.js
+++ 6_requests-suspense/6_5/src/post/Post.js
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { ThemeContext } from "../contexts";
 
-export default function Post({ title, content, author }) {
+function Post({ title, content, author }) {
   const { secondaryColor } = useContext(ThemeContext);
   return (
     <>
@@ -14,3 +14,5 @@ export default function Post({ title, content, author }) {
     </>
   );
 }
+
+export default React.memo(Post);
