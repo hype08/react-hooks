64c97197ff4b07562884d14b96c2f16fbc915b0b
henry <tarou.88@gmail.com>
2020-01-26 09:31:45 +0900

    6_3 Implement Login

--- 6_requests-suspense/6_3/src/user/Login.js
+++ 6_requests-suspense/6_3/src/user/Login.js
@@ -7,6 +7,24 @@ export default function Login() {
   const [loginFailed, setLoginFailed] = useState(false);
   const [password, setPassword] = useState("");
   const { dispatch } = useContext(StateContext);
+  const [user, login] = useResource((username, password) => ({
+    url: `/login/${encodeURI(username)}/${encodeURI(password)}`,
+    method: "get"
+  }));
+
+  useEffect(() => {
+    if (user && user.data) {
+      if (user.data.length > 0) {
+        setLoginFailed(false);
+        dispatch({ type: "LOGIN", username: user.data[0].username });
+      } else {
+        setLoginFailed(true);
+      }
+    }
+    if (user && user.error) {
+      setLoginFailed(true);
+    }
+  }, [user]);
 
   function handleUsername(e) {
     setUsername(e.target.value);
@@ -20,7 +38,7 @@ export default function Login() {
     <form
       onSubmit={e => {
         e.preventDefault();
-        dispatch({ type: "LOGIN", username });
+        login(username, password);
       }}
     >
       <label htmlFor="login-username">Username: </label>
@@ -35,11 +53,14 @@ export default function Login() {
       <input
         type="password"
         value={password}
-        onChange={handlePassword}
         name="login-password"
         id="login-password"
+        onChange={handlePassword}
       />
       <input type="submit" value="Login" disabled={username.length === 0} />
+      {loginFailed && (
+        <span style={{ color: "red" }}>Invalid username or password</span>
+      )}
     </form>
   );
 }
