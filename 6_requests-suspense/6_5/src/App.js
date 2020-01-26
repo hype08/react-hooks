a84a73809c2ec492b1b726970b32a1b163645200
henry <tarou.88@gmail.com>
2020-01-26 22:01:13 +0900

    init 6_5

---
+++ 6_requests-suspense/6_5/src/App.js
@@ -0,0 +1,71 @@
+import React, { useReducer, useEffect, useState } from "react";
+import { useResource } from "react-request-hook";
+
+import "./App.css";
+import UserBar from "./user/UserBar";
+import CreatePost from "./post/CreatePost";
+import PostList from "./post/PostList";
+import Header from "./Header";
+import ChangeTheme from "./ChangeTheme";
+import { ThemeContext, StateContext } from "./contexts";
+import appReducer from "./reducers";
+
+export default function App() {
+  const [theme, setTheme] = useState({
+    primaryColor: "deepskyblue",
+    secondaryColor: "coral"
+  });
+
+  const [state, dispatch] = useReducer(appReducer, {
+    user: "",
+    posts: [],
+    error: ""
+  });
+
+  const { user, error } = state;
+
+  const [posts, getPosts] = useResource(() => ({
+    url: "/posts",
+    method: "get"
+  }));
+
+  useEffect(getPosts, []);
+
+  // dispatch ERROR message if failure to fetch.
+  // dispatch FETCH_POSTS if data exists, and trigger it every time the post object updates.
+  useEffect(() => {
+    if (posts && posts.error) {
+      dispatch({ type: "POSTS_ERROR" });
+    }
+    if (posts && posts.data) {
+      dispatch({ type: "FETCH_POSTS", posts: posts.data.reverse() }); // newest posts listed first.
+    }
+  }, [posts]);
+
+  useEffect(() => {
+    if (user) {
+      document.title = `${user} - React Hooks Blog`;
+    } else {
+      document.title = `React Hooks Blog`;
+    }
+  }, [user]);
+
+  return (
+    <StateContext.Provider value={{ state, dispatch }}>
+      <ThemeContext.Provider value={theme}>
+        <div style={{ padding: 8 }}>
+          <Header text="React Hooks Blog" />
+          <ChangeTheme theme={theme} setTheme={setTheme} />
+          <br />
+          <UserBar />
+          <br />
+          {user && <CreatePost />}
+          <br />
+          <hr />
+          {error && <b> {error} </b>}
+          <PostList />
+        </div>
+      </ThemeContext.Provider>
+    </StateContext.Provider>
+  );
+}
---
+++ 6_requests-suspense/6_5/src/post/CreatePost.js
@@ -0,0 +1,56 @@
+import React, { useState, useContext } from "react";
+import { useResource } from "react-request-hook";
+import { StateContext } from "../contexts";
+
+export default function CreatePost() {
+  const { state, dispatch } = useContext(StateContext);
+  const { user } = state;
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  // pass post data to the createPost function.
+  // we dont need to store the post in state, so ignore first value of array by not specifying it and putting a comma.
+  const [, createPost] = useResource(({ title, author, content }) => ({
+    url: "/posts",
+    method: "posts",
+    data: { title, author, content }
+  }));
+
+  function handleCreate() {
+    createPost({ title, author: user, content });
+    dispatch({ type: "CREATE_POST", title, content, author: user });
+  }
+
+  function handleTitle(e) {
+    setTitle(e.target.value);
+  }
+
+  function handleContent(e) {
+    setContent(e.target.value);
+  }
+
+  return (
+    <form
+      onSubmit={e => {
+        e.preventDefault();
+        handleCreate();
+      }}
+    >
+      <div>
+        Author: <b>{user}</b>
+      </div>
+      <div>
+        <label htmlFor="create-title">Title: </label>
+        <input
+          type="text"
+          value={title}
+          onChange={handleTitle}
+          name="create-title"
+          id="create-title"
+        />
+      </div>
+      <textarea value={content} onChange={handleContent} />
+      <input type="submit" value="Create" />
+    </form>
+  );
+}
